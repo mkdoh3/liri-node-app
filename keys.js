186d85bb21f9dcecf1ae964e38fc63711a32ddd0
mkdoh3 <mkdoherty3@GMAIL.COM>
2017-10-31 17:58:35 -0500

    added some stuff

--- keys.js
+++ keys.js
@@ -1,10 +1,15 @@
 console.log("keys loaded");
 
-const twitterKeys = {
-    consumer_key: '4FrUchDcxZT3ZLUpHtd9Ofa4T',
-    consumer_secret: 'jXBg1QBhVkLZsVBE4qQbOT6G0egdSpytxcv8GWNSQHKRFWLj2M',
-    access_token_key: '925042263327236096-cckERCLdA3MzgJggowpVi59ycDcZeKF',
-    access_token_secret: 'QvAe7E0UMbkmI3SwPWS0kTtcsQP5oE2UMsKPBbtLIR37W',
+const apiKeys = {
+    twitterKeys: {
+        consumer_key: '4FrUchDcxZT3ZLUpHtd9Ofa4T',
+        consumer_secret: 'jXBg1QBhVkLZsVBE4qQbOT6G0egdSpytxcv8GWNSQHKRFWLj2M',
+        access_token_key: '925042263327236096-cckERCLdA3MzgJggowpVi59ycDcZeKF',
+        access_token_secret: 'QvAe7E0UMbkmI3SwPWS0kTtcsQP5oE2UMsKPBbtLIR37W',
+    },
+    spotifyKeys: {
+        id: "49453bfa169941d0baddfcbf9d6aaf3c",
+        secret: "680dee25c00b43e3a7f4104f4bff5328"
+    }
 }
-
-module.exports = twitterKeys;
+module.exports = apiKeys;
--- liri-app.js
+++ liri-app.js
@@ -1,34 +1,188 @@
 const keys = require("./keys.js");
-console.log("twitter keys", keys)
+//console.log(keys)
 const inquirer = require("inquirer");
-const Twitter = require('twitter');
-const client = new Twitter(keys);
+const Twitter = require("twitter");
+const twitterClient = new Twitter(keys.twitterKeys);
+const TextAnimation = require("text-animation")
+const Spotify = require('node-spotify-api');
+const spotifyClient = new Spotify(keys.spotifyKeys)
 
 
+function initGreeting() {
+    TextAnimation({
+        text: "Hello Friend!!",
+        animation: "top-bottom",
+        delay: 30
+    }, function (err) {
+        if (err) {
+            throw err
+        }
+        process.stdout.write('\033c');
+        mainMenu("Hey! Hi! How are ya? What can I do ya for??");
 
-inquirer.prompt([
-    {
-        type: "list",
-        message: "Whatchyou want?!",
-        choices: ["See Tweets", "Search Spotify", "Search Movie Database", "Runs Commands.txt"],
-        name: "commandInput"
+    })
+}
+
+
+function mainMenu(msg) {
+    inquirer.prompt([
+        {
+            type: "list",
+            message: msg,
+            choices: ["See Tweets", "Search Spotify", "Search Movie Database", "Run Commands.txt", "Exit :("],
+            name: "choice"
     }
 ]).then(function (response) {
-    console.log(response.commandInput)
-    if (response.commandInput === "See Tweets") {
-        getTweets();
-    }
-    //run function corresponding to input 
-
-})
-
-function getTweets() {
-    const params = {
-        screen_name: 'mkdoh3'
-    };
-    client.get('statuses/user_timeline', params, function (error, tweets, response) {
-        if (!error) {
-            console.log(tweets[0].text);
+        //clears screen
+        let r = response.choice
+        process.stdout.write('\033c');
+        console.log(response.choice)
+        if (r === "See Tweets") {
+            tweetChoice();
         }
+        if (r === "Search Spotify") {
+            spotifyChoice();
+        }
+        if (r === "Exit :(") {
+            process.stdout.write('\033c');
+            process.exit();
+        }
+        //run function corresponding to input 
+
     });
+};
+
+
+
+
+
+function tweetChoice() {
+    //    process.stdout.write('\033c');
+    inquirer.prompt([
+        {
+            type: "list",
+            message: "Pick One!",
+            choices: ["My Tweets", "Search User", "Return"],
+            name: "choice"
+        }
+    ]).then(function (response) {
+        process.stdout.write('\033c');
+        if (response.choice === "Return") {
+            mainMenu('Welcome Back!');
+            //I'd like to change this so you can enter in and then save your username maybe?
+        } else if (response.choice === "My Tweets") {
+            getTweets('mkdoh3');
+        } else {
+            inquirer.prompt([
+                {
+                    type: "input",
+                    message: "Twitter Username > ",
+                    name: "user"
+                }
+            ]).then(function (response) {
+                getTweets(response.user)
+            })
+        }
+    })
+};
+
+
+
+function getTweets(userName) {
+    process.stdout.write('\033c');
+    const params = {
+        screen_name: userName,
+        count: 5
+    };
+    twitterClient.get('statuses/user_timeline', params, function (error, tweets, response) {
+        if (!error) {
+            //can you make template strings not look horrendous??
+            if (tweets.length === 0) {
+                console.log('This lameazoid has never tweeted! SAD! \nLets try again :D\n')
+                tweetChoice()
+            } else {
+
+                console.log(`            @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+                    ${tweets[0].user.name}      
+            @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@`)
+                tweets.forEach(function (e) {
+                    console.log(`
+        <<---- ${e.created_at} ---->>
+  "${e.text}"
+            <<-- RT: ${e.retweet_count} --- Favs: ${e.favorite_count} -->>
+`)
+                })
+                mainMenu("Where to now??");
+            }
+        } else {
+            console.log(`
+<-- Why you gotta do me like that? Enter a valid username or GTheckO! -->
+`)
+            tweetChoice();
+        }
+    })
+
+};
+
+
+
+function spotifyChoice() {
+    inquirer.prompt([
+        {
+            type: "list",
+            message: "Pick One!",
+            choices: ["Search Song", "Top Ten", "Return"],
+            name: "choice"
+        }
+    ]).then(function (response) {
+        process.stdout.write('\033c');
+        if (response.choice === "Return") {
+            mainMenu('Welcome Back!');
+        }
+        if (response.choice === "Search Song") {
+            spotifySearch();
+        }
+    })
 }
+
+
+
+function spotifySearch() {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "Enter Song Name > ",
+            name: "choice"
+        }
+    ]).then(function (response) {
+
+        spotifyClient
+            .search({
+                type: 'track',
+                query: response.choice,
+                limit: 3
+            })
+            .then(function (response) {
+                console.log(response.tracks.items)
+                response.tracks.items.forEach(function (e) {
+
+                    console.log("song", e.name)
+                    console.log("album", e.album.name);
+                    console.log("artist", e.artists.name)
+                })
+            })
+            .catch(function (err) {
+                console.log(err);
+            })
+    })
+}
+
+
+
+
+
+
+
+
+
+initGreeting();
