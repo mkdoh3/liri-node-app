19866e3563086b9586b9d072f0dab36dd6c05015
mkdoh3 <mkdoherty3@GMAIL.COM>
2017-11-01 17:07:34 -0500

    added spotify search functionality

--- liri-app.js
+++ liri-app.js
@@ -1,16 +1,15 @@
 const keys = require("./keys.js");
-//console.log(keys)
 const inquirer = require("inquirer");
 const Twitter = require("twitter");
-const twitterClient = new Twitter(keys.twitterKeys);
-const TextAnimation = require("text-animation")
 const Spotify = require('node-spotify-api');
+const TextAnimation = require("text-animation")
+const twitterClient = new Twitter(keys.twitterKeys);
 const spotifyClient = new Spotify(keys.spotifyKeys)
 
 
 function initGreeting() {
     TextAnimation({
-        text: "Hello Friend!!",
+        text: "H e l l o\n Friend!",
         animation: "top-bottom",
         delay: 30
     }, function (err) {
@@ -22,7 +21,6 @@ function initGreeting() {
 
     })
 }
-
 
 function mainMenu(msg) {
     inquirer.prompt([
@@ -45,16 +43,23 @@ function mainMenu(msg) {
         }
         if (r === "Exit :(") {
             process.stdout.write('\033c');
-            process.exit();
+            //            process.exit();
+            TextAnimation({
+                text: "B y e\n  forever!",
+                animation: "bottom-top",
+                delay: 30
+            }, function (err) {
+                if (err) {
+                    throw err
+                }
+                process.stdout.write('\033c');
+                process.exit();
+            })
         }
         //run function corresponding to input 
 
     });
 };
-
-
-
-
 
 function tweetChoice() {
     //    process.stdout.write('\033c');
@@ -85,7 +90,6 @@ function tweetChoice() {
         }
     })
 };
-
 
 
 function getTweets(userName) {
@@ -125,56 +129,83 @@ function getTweets(userName) {
 };
 
 
-
 function spotifyChoice() {
     inquirer.prompt([
         {
             type: "list",
             message: "Pick One!",
-            choices: ["Search Song", "Top Ten", "Return"],
+            choices: ["Search Song", "Search Artist", "Return"],
             name: "choice"
         }
     ]).then(function (response) {
         process.stdout.write('\033c');
         if (response.choice === "Return") {
             mainMenu('Welcome Back!');
-        }
-        if (response.choice === "Search Song") {
-            spotifySearch();
+        } else if (response.choice === "Search Song") {
+            spotifySearch("Song", "track");
+        } else {
+            spotifySearch("artist", "artist");
         }
     })
 }
 
 
-
-function spotifySearch() {
+function spotifySearch(msg, callType) {
     inquirer.prompt([
         {
             type: "input",
-            message: "Enter Song Name > ",
+            message: msg + " name: ",
             name: "choice"
         }
     ]).then(function (response) {
-
-        spotifyClient
-            .search({
-                type: 'track',
-                query: response.choice,
-                limit: 3
-            })
-            .then(function (response) {
-                console.log(response.tracks.items)
-                response.tracks.items.forEach(function (e) {
-
-                    console.log("song", e.name)
-                    console.log("album", e.album.name);
-                    console.log("artist", e.artists.name)
-                })
-            })
-            .catch(function (err) {
-                console.log(err);
-            })
+        process.stdout.write('\033c');
+        spotifyCall(callType, response.choice)
     })
+}
+
+
+function spotifyCall(type, query) {
+    spotifyClient.search({
+        type: type,
+        query: query,
+        limit: 50
+    }).then(function (response) {
+        if (type === 'track') {
+            songResults(response)
+        } else {
+            artistResults(response);
+        }
+    }).catch(function (err) {
+        console.log(err);
+    })
+}
+
+function songResults(response) {
+    let results = response.tracks.items
+    if (results.length < 1) {
+        console.log("   Y'all messin' with me?? No Results Found! Try again\n")
+        spotifyChoice();
+    } else {
+        console.log("************** RESULTS ********************")
+        console.log("\n<=== Only Full Albums Shown, no 'Singles' ===>\n")
+        for (let i = 0; i < 10; i++) {
+            if (results[i].album.album_type === 'album') {
+                console.log("\266 \266 \266 \266 \266 \266 \266 \266 \266 \266 \266 \266 \266 \266 \266 \266")
+                console.log(`  Artist: ${results[i].artists[0].name}
+  Song: ${results[i].name}
+  Album: ${results[i].album.name}
+  Preview: ${results[i].preview_url}`)
+                console.log("\266 \266 \266 \266 \266 \266 \266 \266 \266 \266 \266 \266 \266 \266 \266 \266\n\n")
+            }
+        }
+        mainMenu("Where to now??");
+    }
+}
+
+function artistResults(response) {
+    console.log("******** UNDER CONSTRUCTION **********")
+    console.log(response)
+    console.log("******** UNDER CONSTRUCTION **********")
 }
 
 
